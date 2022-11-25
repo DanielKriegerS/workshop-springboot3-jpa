80903376f2656f96e755b8be67d1ab1f027f718e
DanielKriegerS <danielschifelbain@gmail.com>
2022-11-25 14:44:23 -0300

    Category

---
+++ course/src/main/java/com/danielkrieger/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.danielkrieger.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danielkrieger.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
